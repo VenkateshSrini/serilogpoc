18dc187792f148f239f2453e45c9dfc1dae02778
VenkateshSrinivasan <venkatesh-5.srinivasan-5@cognizant.com>
2020-07-21 20:31:23 +0530

    Base commit

---
+++ serilogFilepoc/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Diagnostics;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Microsoft.Extensions.DependencyInjection;
+using serilogFilepoc.Service;
+
+namespace serilogFilepoc
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var isConsole = (Debugger.IsAttached || args.Contains("--console"));
+            var host = new HostBuilder()
+                            .ConfigureAppConfiguration((hostingContext, config) =>
+                            {
+                                config.AddJsonFile("appsettings.json", optional: true);
+                                config.AddEnvironmentVariables();
+                                if (args != null)
+                                {
+                                    config.AddCommandLine(args);
+                                }
+
+
+                            })
+                            .ConfigureLogging((hostingContext, loggingBuilder) =>
+                            {
+                                loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                                loggingBuilder.AddConsole();
+                                Log.Logger = new LoggerConfiguration()
+                                    .ReadFrom.Configuration(hostingContext.Configuration).CreateLogger();
+                                loggingBuilder.AddSerilog(dispose: true);
+                            })
+                            .ConfigureServices((hostContext, services) =>
+                            {
+                                services.AddHostedService<SampleService>();
+                            })
+                            .Build();
+            if (isConsole)
+                await host.RunAsync();
+            else
+            {
+                await host.StartAsync();
+                await host.StopAsync();
+                host.Dispose();
+            }
+        }
+    }
+}
---
+++ serilogFilepoc/Service/SampleService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace serilogFilepoc.Service
+{
+    public class SampleService : BackgroundService
+    {
+        private readonly ILogger<BackgroundService> logger;
+        public SampleService(ILogger<BackgroundService> logger)
+        {
+            this.logger = logger;
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            logger.LogInformation("Task invoked");
+            return Task.CompletedTask;
+        }
+    }
+}
